6ba80f4326f348661430ce5996842f238773b6dd
zakisista <zakisista00@gmail.com>
2022-09-19 18:06:49 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFGvj2xIXUAiAJlfTDjNfGkjP0gXzOvi4",
+  authDomain: "listr-690a4.firebaseapp.com",
+  projectId: "listr-690a4",
+  storageBucket: "listr-690a4.appspot.com",
+  messagingSenderId: "876564251500",
+  appId: "1:876564251500:web:11ca0d99fdbca92cce2949"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
